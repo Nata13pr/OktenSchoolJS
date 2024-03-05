fc18df4e8d8c53a166ed2a38dee630bdf0b703a0
Nata13pr <Nata13pr@gmail.com>
2024-03-05 18:47:18 +0200

    feat:homework6

--- homework5/task1.js
+++ homework5/task1.js
@@ -175,3 +175,7 @@ const exchange = (sumUAH, currencyValues, exchangeCurrency) => {
 }
 
 exchange(10000, [{currency: 'USD', value: 40}, {currency: 'EUR', value: 42}], 'USD')
+
+const l=['RRRRRR'];
+
+console.log(typeof l)
